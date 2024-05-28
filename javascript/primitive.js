55bec2614fc01229bbcdc8d718b4431bccf10e05
anjaemun <ajm0111@naver.com>
2024-05-28 18:05:32 +0900

    primitive, reference, DataType

---
+++ javascript/primitive.js
@@ -0,0 +1,17 @@
+// 원시 타입(primitive type)
+
+let number = 1; // 숫자형(number)
+let str = "abc";    // 문자열형(String)
+let bool = true;    // 논리형(boolean)
+let nul = null;   // null
+let undi = undefined;    // undefined
+let symbol = Symbol();    // Symbol
+
+
+console.log(typeof(1));
+console.log(typeof('abc'));
+console.log(typeof(true));
+console.log(typeof(null));
+console.log(typeof(undefined));
+console.log(typeof(Symbol()));
+
---
+++ javascript/reference.js
@@ -0,0 +1,9 @@
+// 참조 타입(reference type)
+
+let array = []; // 배열(array)
+let obj = {};    // 객체(object)
+let func = function(){};    // 함수(function)
+
+console.log(typeof([]));
+console.log(typeof({}));
+console.log(typeof(function(){}));
