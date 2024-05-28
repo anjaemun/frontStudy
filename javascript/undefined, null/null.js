3f434f7dc5d7b8ac20025512c97bcdde762f3c11
anjaemun <ajm0111@naver.com>
2024-05-28 19:18:18 +0900

    undefined, null

---
+++ javascript/undefined, null/null.js
@@ -0,0 +1,6 @@
+let str;
+console.log(str);
+
+// 의도적으로 메모리를 비워놓음. 직접 null이라고 할당해야함.
+let num = null;
+console.log(num);
---
+++ javascript/undefined, null/undefined.js
@@ -0,0 +1,3 @@
+// 선언만 되고 값 할당하지 않았을 경우 undefined값 자동 할당 
+let num;
+console.log(num);
