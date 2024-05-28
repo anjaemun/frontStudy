d2bf2bbade0dbaa3b7d1a3c83f70257f428c9f3e
anjaemun <ajm0111@naver.com>
2024-05-28 19:22:21 +0900

    booleanType

---
+++ javascript/booleanType/boolean_1.js
@@ -0,0 +1,7 @@
+// 직접 true, false를 할당 가능 
+
+let bool1 = true;
+console.log(bool1);
+
+let bool2 = false;
+console.log(bool2);
---
+++ javascript/booleanType/boolean_2.js
@@ -0,0 +1,6 @@
+// 논리식으로도 할당 가능 
+let bool1 = 5 < 7; //침 
+console.log(bool1);
+
+let bool2 = 7 > 10; // 거짓 
+console.log(bool2);
